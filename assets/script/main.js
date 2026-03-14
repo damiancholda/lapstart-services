d343f1429063f3236fe5768050ba6d266ed0a4b2
Damian <damian@MacBook-Pro-Damian.local>
2026-03-14 02:16:40 +0100

    uslugi chaneges

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,9 +1,32 @@
+
+const navbar = document.querySelector('.navbar');
+const hero = document.querySelector('.hero');
 const navLinks = document.querySelector('.navlinks');
 const burger = document.querySelector('.burger');
 
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                navbar.classList.add('onscroll');
+            } else {
+                navbar.classList.remove('onscroll');
+                burger.classList.remove('active');
+                navLinks.classList.remove('active');
+            }
+        });
+    },
+    {
+        threshold: 0.1,
+        rootMargin: "0px 0px -50px 0px"
+    }
+);
+
+observer.observe(hero);
+
 burger.addEventListener('click',()=>{
-    burger.classList.toggle('active');
-    navLinks.classList.toggle('active');
+        burger.classList.toggle('active');
+        navLinks.classList.toggle('active');
 
     navLinks.addEventListener('click',()=>{
         if(burger.classList.contains('active')){
@@ -11,4 +34,5 @@ burger.addEventListener('click',()=>{
             navLinks.classList.remove('active');
         }
     })
+    
 })
